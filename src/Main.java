15b6da471cc19e4e6e090026665d102da17a4df9
Daniel <Daniel.kotrba@gmx.at>
2020-02-17 09:36:02 +0100

    Add Fizzbuzz

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        for (int number = 1; number <= 100; number++) {
+            if (number % 3 == 0 && number % 5 == 0)
+                System.out.println("fizzbuzz");
+            else if (number % 5 == 0)
+                System.out.println("fizz");
+            else if (number % 3 == 0)
+                System.out.println("buzz");
+            else
+                System.out.println(number);
+        }
+
+
+    }
+}
